e5079e131183a7b3ffc050beccce7e4a6749e946
rona wang <rona.y.wang@gmail.com>
2021-07-20 16:11:44 -0700

    made story modal component

--- src/components/AllStoriesPage.js
+++ src/components/AllStoriesPage.js
@@ -1,10 +1,34 @@
+import Modal from '@material-ui/core/Modal';
+import { useState } from 'react'
+import StoryPage from "./StoryPage"
+
 const AllStoriesPage = ({ stories }) => {
     const Thumbnail = ({ story }) => {
+        const [open, setOpen] = useState(false);
+        const handleOpen = () => {
+            setOpen(true);
+        };
+        const handleClose = () => {
+            setOpen(false);
+        };
         return (
-            <div>
+            
+        <div>
+
+            <div onclick= {handleOpen}>
                 <img src={story.image} />
                 <h1>{story.name}</h1>
+
             </div>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description"
+            >
+              <StoryPage story = {story} />
+            </Modal>
+        </div>
 
         )
     }
@@ -18,6 +42,7 @@ const AllStoriesPage = ({ stories }) => {
             <h1>Here is the list of stories!</h1>
             {storyThumbnails}
         </div>
+
     )
 }
 
