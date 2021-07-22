072305d2bc679cac43eb72a0fcc3f393a23a1fd4
ifeomi <omidiran@college.harvard.edu>
2021-07-22 19:00:37 -0400

    move link

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,10 @@ const About = () => {
     return (
         <div className="about-text">
             <h1>
-                <a href="https://gangscoalition.org/">G.A.N.G.S. Coalition</a> Story Initiative
+                G.A.N.G.S. Coalition Story Initiative
             </h1>
             <p>
-                A common issue that the GANGS coalition faces is the public perception of gang members. In the collective unconscious of Americans nationwide, gang members are coded as violent, dangerous, and a nuisance. The NYPD has taken this cultural signifier - ‘gang member’ - and twisted its parameters so that anybody in the city could be a suspected gang member simply by appearing in their Criminal Group Database. Most people in the Criminal Group Database wind up there based on unsubstantiated hunches like social media activity, place of residence, and even the color of their clothing in online pictures. The only difference between this and “Stop and Frisk” is that the arbiters are algorithms instead of officers. 
+                A common issue that the <a href="https://gangscoalition.org/">GANGS coalition</a> faces is the public perception of gang members. In the collective unconscious of Americans nationwide, gang members are coded as violent, dangerous, and a nuisance. The NYPD has taken this cultural signifier - ‘gang member’ - and twisted its parameters so that anybody in the city could be a suspected gang member simply by appearing in their Criminal Group Database. Most people in the Criminal Group Database wind up there based on unsubstantiated hunches like social media activity, place of residence, and even the color of their clothing in online pictures. The only difference between this and “Stop and Frisk” is that the arbiters are algorithms instead of officers. 
             </p>
         </div>
     )
