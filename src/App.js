7fb1e24d0a943812226f6a5969c9c1599aba314a
ifeomi <omidiran@college.harvard.edu>
2021-07-22 19:02:08 -0400

    change spaces back

--- src/App.js
+++ src/App.js
@@ -11,44 +11,44 @@ import './lib/jquery.pagepiling';
 import './lib/jquery.pagepiling.css';
 
 function App() {
-  const [stories, setStories] = useState()
+    const [stories, setStories] = useState()
 
-  useEffect(() => {
-      Firebase
-          .firestore()
-          .collection('stories')
-          .get()
-          .then((querySnapshot) => {
-                  if (querySnapshot.docs) {
-                      setStories(querySnapshot.docs.map(d => d.data()))
-                  }
-              }
-          )
-  }, [])
+    useEffect(() => {
+        Firebase
+            .firestore()
+            .collection('stories')
+            .get()
+            .then((querySnapshot) => {
+                    if (querySnapshot.docs) {
+                        setStories(querySnapshot.docs.map(d => d.data()))
+                    }
+                }
+            )
+    }, [])
 
-  // initialize pagepiling
-  useEffect(() => {
-    $(function() {
-      $('#pagepiling').pagepiling();
-    });
-  })
-  
-  return stories ? (
-      <div id="pagepiling" className="App">
-        <div className="section sec1">
-          <About />
+    // initialize pagepiling
+    useEffect(() => {
+      $(function() {
+        $('#pagepiling').pagepiling();
+      });
+    })
+    
+    return stories ? (
+        <div id="pagepiling" className="App">
+          <div className="section sec1">
+            <About />
+          </div>
+          <div className="section pp-scrollable ">
+            <AllStoriesPage stories={stories} />
+          </div>
+          <div className="section pp-scrollable">
+            <StoryPage story={stories.filter(function(obj) {
+              return obj.approved
+            })[0]} />
+          </div>
         </div>
-        <div className="section pp-scrollable ">
-          <AllStoriesPage stories={stories} />
-        </div>
-        <div className="section pp-scrollable">
-          <StoryPage story={stories.filter(function(obj) {
-            return obj.approved
-          })[0]} />
-        </div>
-      </div>
-    ) :
-    "loading..."
+      ) :
+      "loading..."
 }
 
 export default App
