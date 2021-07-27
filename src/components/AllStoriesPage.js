6676ed5d79451f6160f3ad4d2885b75396ecd32a
ifeomi <omidiran@college.harvard.edu>
2021-07-26 20:58:03 -0400

    add modal to view full story (thank you rona and ali)

--- src/components/AllStoriesPage.js
+++ src/components/AllStoriesPage.js
@@ -1,13 +1,28 @@
+import { useState } from 'react'
+import Modal from '@material-ui/core/Modal'
+import StoryPage from './StoryPage'
 const AllStoriesPage = ({ stories }) => {
     const Thumbnail = ({ story }) => {
+        const [open, setOpen] = useState(false);
+        const handleOpen = () => {
+            setOpen(true);
+        };
+        const handleClose = () => {
+            setOpen(false);
+        };
+
         return (
             <div key={story.id}>
                 <img src={story.image} alt={story.name + " image"} className="image" />
                 <h1>{story.name}</h1>
+                <input type="button" onClick={handleOpen} value="Open Story" />
+                <Modal open={open} onClose={handleClose}>
+                    <StoryPage story={story} />
+                </Modal>
             </div>
-
         )
     }
+
     let storyThumbnails = []
     for (let i = 0; i < stories.length; i++) {
         storyThumbnails.push(<Thumbnail story={stories[i]} />)
@@ -20,5 +35,4 @@ const AllStoriesPage = ({ stories }) => {
         </div>
     )
 }
-
 export default AllStoriesPage
