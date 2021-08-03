112e845ec6d83887bb3ada646b127adcaa6240ea
Anna Flores <anna.flores@ro.co>
2021-08-02 18:03:34 -0700

    added initial grid for gallery of stories. logic is needed to make our design a reality...

--- src/components/AllStoriesPage.js
+++ src/components/AllStoriesPage.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import Modal from '@material-ui/core/Modal'
+import Grid from '@material-ui/core/Grid';
 import StoryPage from './StoryPage'
-const AllStoriesPage = ({ stories }) => {
-    const Thumbnail = ({ story }) => {
+
+const AllStoriesPage = ({stories}) => {
+    const Thumbnail = ({story}) => {
         const [open, setOpen] = useState(false);
         const handleOpen = () => {
             setOpen(true);
@@ -13,10 +15,10 @@ const AllStoriesPage = ({ stories }) => {
 
         return (
             <div key={story.id}>
-                <StoryPage story={story} showTitle={false} showText={false} />
-                <input type="button" onClick={handleOpen} value="Open Story" />
+                <StoryPage story={story} showTitle={false} showText={false}/>
+                <input type="button" onClick={handleOpen} value="Open Story"/>
                 <Modal open={open} onClose={handleClose}>
-                    <StoryPage story={story} showTitle={false} showText={true} />
+                    <StoryPage story={story} showTitle={false} showText={true}/>
                 </Modal>
             </div>
         )
@@ -24,13 +26,27 @@ const AllStoriesPage = ({ stories }) => {
 
     let storyThumbnails = []
     for (let i = 0; i < stories.length; i++) {
-        storyThumbnails.push(<Thumbnail story={stories[i]} />)
+        storyThumbnails.push(<Thumbnail story={stories[i]}/>)
     }
 
     return (
         <div>
             <h1>All Stories</h1>
-            {storyThumbnails}
+            <div>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={3}
+                >
+                    <Grid item xs={10}>
+                        <h1> hello </h1>
+                    </Grid>
+                    {storyThumbnails}
+                </Grid>>
+            </div>
+
         </div>
     )
 }
